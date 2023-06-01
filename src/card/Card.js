2cfbe90fbbe10fdee09a25834f4e098ef7aa4c99
IGNACIO MATIAS LOPEZ ABRAHAM <46872351@ort>
2023-06-01 10:16:14 -0300

    tidy up

--- src/card/Card.js
+++ src/card/Card.js
@@ -8,13 +8,13 @@ function Card({cita, setDel}) {
     }
 
     return(
-        <div class="cita">
+        <div className="cita">
             <p>Mascota: <span>{cita.mascota}</span></p>
             <p>Dueño: <span>{cita.dueño}</span></p>
             <p>Fecha: <span>{cita.fecha}</span></p>
             <p>Hora: <span>{cita.hora}</span></p>
             <p>Sintomas: <span>{cita.sintomas}</span></p>
-            <button class="button eliminar u-full-width" onClick={() => handleClick()}>Eliminar ×</button>
+            <button className="button eliminar u-full-width" onClick={() => handleClick()}>Eliminar ×</button>
         </div>
     );
 }
